6a847538226cea5f114e8a4e7473412f16edbeb2
subramanian <subramanian.kumarappan@halliburton.com>
2023-12-24 10:31:48 +0530

    added node class

---
+++ DSA/DSA/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    public class Node
+    {
+        public Node link;
+        public int value;
+
+        public Node(int value) 
+        {
+            this.link = null;
+            this.value = value;     
+        }   
+    }
+}
--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Node node = new Node(5);
+            Console.WriteLine(node.value);
         }
     }
 }
